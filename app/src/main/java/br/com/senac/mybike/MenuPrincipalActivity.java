99aa92cadd3e339a656e644214fbcfc301ae5c0d
AnnaC4tharina <annacatharina480@gmail.com>
2022-06-30 16:26:45 -0300

    Criando o menu com cardView

--- app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/senac/mybike/MenuPrincipalActivity.java
@@ -1,13 +1,14 @@
 package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_principal_layout);
     }
 }
